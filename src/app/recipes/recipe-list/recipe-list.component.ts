7a7591db0f8c840ae60508658cda3b0dc5ce25a5
Dylan Hague <haguedylan@gmail.com>
2020-12-02 09:19:21 -0500

    Create Recipe-List Component; Populate 2 Example Recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+	recipes: Recipe[] = [
+		new Recipe('A Test Recipe', 'This is simply a test','https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Dak-gangjeong.jpg/800px-Dak-gangjeong.jpg'),
+		new Recipe('A Test Recipe', 'This is simply a test','https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Dak-gangjeong.jpg/800px-Dak-gangjeong.jpg')
+	];
   constructor() { }
 
   ngOnInit(): void {
