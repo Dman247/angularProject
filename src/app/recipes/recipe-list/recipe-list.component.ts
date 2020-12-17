322599c78436dad80d1bb43d0ed2ff6c8d39a6d2
Dylan Hague <haguedylan@gmail.com>
2020-12-17 15:41:35 -0500

    Create Recipe Service
    Replacing the existing component event binding interplay that was inplace with RecipeService.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,18 +8,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-	@Output() recipeWasSelected = new EventEmitter<Recipe>();
-	recipes: Recipe[] = [
-		new Recipe('A Test Recipe', 'This is simply a test','https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Dak-gangjeong.jpg/800px-Dak-gangjeong.jpg'),
-		new Recipe('Another Test Recipe', 'This is simply a test','https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Dak-gangjeong.jpg/800px-Dak-gangjeong.jpg')
-	];
-  constructor() { }
+	recipes: Recipe[];
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+		this.recipes = this.recipeService.getRecipes();    
   }
-
-	onRecipeSelected(recipe: Recipe) {
-		this.recipeWasSelected.emit(recipe);
-	}
-
 }
