886d47da91d7f9961ed9dde99bfa59c8b4851c18
Dylan Hague <haguedylan@gmail.com>
2020-12-17 17:33:26 -0500

    Add Ingredients to Recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,23 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
 recipeSelected = new EventEmitter<Recipe>();
 
 	private recipes: Recipe[] = [
-		new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Dak-gangjeong.jpg/800px-Dak-gangjeong.jpg'),
-		new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Dak-gangjeong.jpg/800px-Dak-gangjeong.jpg')
+		new Recipe('A Test Recipe',
+		'This is simply a test',
+		'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Dak-gangjeong.jpg/800px-Dak-gangjeong.jpg',
+		[
+			new Ingredient('Meat', 1),
+			new Ingredient('French Fries', 20)
+		]),
+		new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Dak-gangjeong.jpg/800px-Dak-gangjeong.jpg',
+		[
+			new Ingredient('Buns', 2),
+			new Ingredient('Meat', 1)
+		])
 	];
 	
 	getRecipes() {
