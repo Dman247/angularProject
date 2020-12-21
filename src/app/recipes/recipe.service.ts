6e01574e788ede10cbd7e0ad41d6dca7e3e53df9
Dylan Hague <haguedylan@gmail.com>
2020-12-21 14:21:46 -0500

    Feature: Added Ability to send a Recipe to the Shopping List.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,10 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
 recipeSelected = new EventEmitter<Recipe>();
 
@@ -19,8 +22,14 @@ recipeSelected = new EventEmitter<Recipe>();
 			new Ingredient('Meat', 1)
 		])
 	];
+
+	constructor(private slService: ShoppingListService) {}
 	
 	getRecipes() {
 		return this.recipes.slice();
+	}
+
+	addIngredientsToShoppingList(ingredients: Ingredient[]){
+		this.slService.addIngredients(ingredients);
 	}
 }
