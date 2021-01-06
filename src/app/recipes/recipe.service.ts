5b3fe27d8f5aff25e223defdf201d78544c41e6a
Dylan Hague <haguedylan@gmail.com>
2021-01-06 09:15:57 -0500

    Changing Second Demo Image for Clarity

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -16,7 +16,7 @@ recipeSelected = new EventEmitter<Recipe>();
 			new Ingredient('Meat', 1),
 			new Ingredient('French Fries', 20)
 		]),
-		new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Dak-gangjeong.jpg/800px-Dak-gangjeong.jpg',
+		new Recipe('Another Test Recipe', 'This is simply a test', 'https://img.theculturetrip.com/450x/smart/images/56-3621217-1422904501802eea9af7ee4032a1c59daecee00008.jpg',
 		[
 			new Ingredient('Buns', 2),
 			new Ingredient('Meat', 1)
