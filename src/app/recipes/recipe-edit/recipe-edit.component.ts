5624a675bfa5fdcb0474596a9614cd92910b6251
Dylan Hague <haguedylan@gmail.com>
2021-01-11 16:10:15 -0500

    Feat: Add and Update Recipes

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -29,7 +30,11 @@ export class RecipeEditComponent implements OnInit {
 	}
 
 	onSubmit() {
-		console.log(this.recipeForm);
+		if (this.editMode) {
+			this.recipeServvice.updateRecipe(this.id, this.recipeForm.value);
+		} else {
+			this.recipeServvice.addRecipe(this.recipeForm.value);
+		}
 	}
 
 	onAddIngredient() {
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -15,7 +15,10 @@ export class RecipeListComponent implements OnInit {
 							private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-		this.recipes = this.recipeService.getRecipes();    
+		this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
+			this.recipes = recipes;
+		})
+		this.recipes = this.recipeService.getRecipes();
   }
 
 	onNewRecipe() {
