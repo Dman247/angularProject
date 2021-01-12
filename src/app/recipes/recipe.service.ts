388833974add7290e15430f08b09aabd975e59b1
Dylan Hague <haguedylan@gmail.com>
2021-01-12 16:08:06 -0500

    Stop Initializing Recipes Array with Hardcoded Values

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,20 +9,22 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
 	recipesChanged = new Subject<Recipe[]>();
 
-	private recipes: Recipe[] = [
-		new Recipe('A Test Recipe',
-		'This is simply a test',
-		'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Dak-gangjeong.jpg/800px-Dak-gangjeong.jpg',
-		[
-			new Ingredient('Meat', 1),
-			new Ingredient('French Fries', 20)
-		]),
-		new Recipe('Another Test Recipe', 'This is simply a test', 'https://img.theculturetrip.com/450x/smart/images/56-3621217-1422904501802eea9af7ee4032a1c59daecee00008.jpg',
-		[
-			new Ingredient('Buns', 2),
-			new Ingredient('Meat', 1)
-		])
-	];
+	// private recipes: Recipe[] = [
+	// 	new Recipe('A Test Recipe',
+	// 	'This is simply a test',
+	// 	'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Dak-gangjeong.jpg/800px-Dak-gangjeong.jpg',
+	// 	[
+	// 		new Ingredient('Meat', 1),
+	// 		new Ingredient('French Fries', 20)
+	// 	]),
+	// 	new Recipe('Another Test Recipe', 'This is simply a test', 'https://img.theculturetrip.com/450x/smart/images/56-3621217-1422904501802eea9af7ee4032a1c59daecee00008.jpg',
+	// 	[
+	// 		new Ingredient('Buns', 2),
+	// 		new Ingredient('Meat', 1)
+	// 	])
+	// ];
+	private recipes: Recipe[] = [];
+
 
 	constructor(private slService: ShoppingListService) {}
 	
