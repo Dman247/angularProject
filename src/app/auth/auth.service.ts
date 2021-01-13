62c3c60917300011abfd64a45d57236d5211ab38
Dylan Hague <haguedylan@gmail.com>
2021-01-13 16:16:17 -0500

    feat: Login Method; Refactor Error Handling in the Auth Component

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-interface AuthResponseData {
+export interface AuthResponseData {
 	kind: string;
 	idToken: string;
 	email: string;
 	refreshToken: string;
 	expiresIn: string;
 	localId: string;
+	registered?: boolean;
 }
 
 @Injectable({
@@ -30,7 +31,7 @@ export class AuthService {
 			)
 			.pipe(catchError(errorRes => {
 				let errorMessage = 'An unkown error occurred!';
-				if (!errorRes.error || !errorRes.error.error){
+				if (!errorRes.error || !errorRes.error.error) {
 					return throwError(errorMessage);
 				}
 				switch (errorRes.error.error.message) {
@@ -40,4 +41,16 @@ export class AuthService {
 				return throwError(errorMessage);
 			}));
 	}
+
+	login(email: string, password: string) {
+		return this.http
+			.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBcOdOuAlsZOAjRck38SWLBQGgyyBqv9FU',
+				{
+					email: email,
+					password: password,
+					returnSecureToken: true,
+				}
+			);
+	}
+
 }
