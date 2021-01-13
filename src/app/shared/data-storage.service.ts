fc0ed5ce5d46842b21fa55568fe5506a621de55e
Dylan Hague <haguedylan@gmail.com>
2021-01-13 10:02:09 -0500

    Bug: Loosing Recipes Upon Refreshing; Adding Resolver to Recipe Paths

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from "../recipes/recipe.model";
@@ -18,16 +18,15 @@ export class DataStorageService {
 	}
 
 	fetchRecipes() {
-		this.http.get<Recipe[]>('https://ng-recipe-book-6ae8d-default-rtdb.firebaseio.com/recipes.json')
+		return this.http.get<Recipe[]>('https://ng-recipe-book-6ae8d-default-rtdb.firebaseio.com/recipes.json')
 		.pipe(map( (recipes: Recipe[]) => {
 			return recipes.map( (recipe: Recipe) => {
 				return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
 			});
-		}))
-		.subscribe(
-			(recipes) => {
-				this.recipeService.setRecipes(recipes);
+		}),
+		tap(recipes => {
+			this.recipeService.setRecipes(recipes);
 			}
-		)
+		))
 	}
 }
