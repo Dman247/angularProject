3ff73ea8676ab419c021916b76ea40bbadc3b87a
Dylan Hague <haguedylan@gmail.com>
2021-01-13 15:19:42 -0500

    feat: Loading Spinner on Auth Form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,6 +9,7 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent implements OnInit {
 	isLoginMode = true;
+	isLoading = false;
 
 	constructor(private authService: AuthService) { }
 
@@ -26,13 +27,16 @@ export class AuthComponent implements OnInit {
 		const email = form.value.email;
 		const password = form.value.password;
 
+		this.isLoading = true;
 		if (this.isLoginMode) {
 			// ....
 		} else {
 			this.authService.signup(email,password).subscribe(resData => {
 				console.log(resData);
+				this.isLoading = false;
 			}, error => {
 				console.log(error);
+				this.isLoading = false;
 			});
 		}
 
