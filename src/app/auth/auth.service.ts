4749036ebc6850b785c716a660f9df3ae7e9c032
Dylan Hague <haguedylan@gmail.com>
2021-01-19 10:15:22 -0500

    Utilize Angular's Environment Directory for the Firebase API Key

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 import { User } from './user.model';
 
@@ -27,7 +28,7 @@ export class AuthService {
 
 	signup(email: string, password: string) {
 		return this.http
-			.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBcOdOuAlsZOAjRck38SWLBQGgyyBqv9FU',
+			.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
 				{
 					email: email,
 					password: password,
@@ -52,7 +53,7 @@ export class AuthService {
 
 	login(email: string, password: string) {
 		return this.http
-			.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBcOdOuAlsZOAjRck38SWLBQGgyyBqv9FU',
+			.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
 				{
 					email: email,
 					password: password,
