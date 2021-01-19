967e92f3afe108b93a1e695368891deb92030881
Dylan Hague <haguedylan@gmail.com>
2021-01-19 15:38:56 -0500

    Implement NgRx for the ShoppingList Component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,34 +1,28 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+	selector: 'app-shopping-list',
+	templateUrl: './shopping-list.component.html',
+	styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-	ingredients: Ingredient[]; 
-	private subscription: Subscription;
+	ingredients: Observable<{ ingredients: Ingredient[] }>;
 
-  constructor(private slService: ShoppingListService) { }
+	constructor(private slService: ShoppingListService, private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>) { }
 
-  ngOnInit(): void {
-		this.ingredients = this.slService.getIngredients();
-		this.subscription = this.slService.ingredientsChanged.subscribe(
-			(ingredients: Ingredient[]) => {
-				this.ingredients = ingredients;
-			}
-		);
+	ngOnInit(): void {
+		this.ingredients = this.store.select('shoppingList');
 	}
 
 	onEditItem(index: number) {
 		this.slService.startedEditing.next(index);
 	}
 
-	ngOnDestroy(): void{
-		this.subscription.unsubscribe();
-	}
-	
+	ngOnDestroy(): void {}
+
 }
