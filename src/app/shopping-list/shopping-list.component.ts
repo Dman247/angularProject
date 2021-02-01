5e0a7dbf8e33bb8649d5a3681541bdd474a060ec
Dylan Hague <haguedylan@gmail.com>
2021-02-01 15:30:17 -0500

    Restructure: Move AppState to Root Level

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,9 +2,9 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
-import * as fromShoppingList from './store/shopping-list.reducer';
-import { Ingredient } from '../shared/ingredient.model';
 import * as shoppingListActions from './store/shopping-list.actions';
+import * as fromApp from '../store/app.reducer';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
 	selector: 'app-shopping-list',
@@ -14,7 +14,7 @@ import * as shoppingListActions from './store/shopping-list.actions';
 export class ShoppingListComponent implements OnInit, OnDestroy {
 	ingredients: Observable<{ ingredients: Ingredient[] }>;
 
-	constructor(private store: Store<fromShoppingList.AppState>) { }
+	constructor(private store: Store<fromApp.AppState>) { }
 
 	ngOnInit(): void {
 		this.ingredients = this.store.select('shoppingList');
