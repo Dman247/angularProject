2a2de3702200072c3789d3b22dadccfc15ccbfd4
Dylan Hague <haguedylan@gmail.com>
2021-02-16 12:33:28 -0500

    Create and Implement AutoLogin Action/Effect

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -7,6 +7,7 @@ import { of } from "rxjs";
 
 import { environment } from "src/environments/environment";
 import * as AuthActions from './auth.actions';
+import { User } from "../user.model";
 
 export interface AuthResponseData {
 	kind: string;
@@ -24,9 +25,9 @@ const handleAuthentication = (
 	userId: string,
 	token: string
 ) => {
-	const expirationDate = new Date(
-		new Date().getTime() + expiresIn * 1000
-	);
+	const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+	const user = new User(email, userId, token, expirationDate);
+	localStorage.setItem('userData', JSON.stringify(user));
 	return new AuthActions.AuthenticateSuccess({
 		email: email,
 		userId: userId,
@@ -37,21 +38,21 @@ const handleAuthentication = (
 
 const handleError = (errorRes: any) => {
 	let errorMessage = 'An unkown error occurred!';
-						if (!errorRes.error || !errorRes.error.error) {
-							return of(new AuthActions.AuthenticateFail(errorMessage));
-						}
-						switch (errorRes.error.error.message) {
-							case 'EMAIL_EXISTS':
-								errorMessage = 'This email exists already.';
-								break;
-							case 'EMAIL_NOT_FOUND':
-								errorMessage = 'This email does not exits.';
-								break;
-							case 'INVALID_PASSWORD':
-								errorMessage = 'This password is not correct.';
-								break;
-						}
-						return of(new AuthActions.AuthenticateFail(errorMessage));
+	if (!errorRes.error || !errorRes.error.error) {
+		return of(new AuthActions.AuthenticateFail(errorMessage));
+	}
+	switch (errorRes.error.error.message) {
+		case 'EMAIL_EXISTS':
+			errorMessage = 'This email exists already.';
+			break;
+		case 'EMAIL_NOT_FOUND':
+			errorMessage = 'This email does not exits.';
+			break;
+		case 'INVALID_PASSWORD':
+			errorMessage = 'This password is not correct.';
+			break;
+	}
+	return of(new AuthActions.AuthenticateFail(errorMessage));
 };
 
 @Injectable()
@@ -109,6 +110,43 @@ export class AuthEffects {
 			this.router.navigate(['/']);
 		})
 	);
+
+	@Effect()
+	autoLogin = this.actions$.pipe(
+		ofType(AuthActions.AUTO_LOGIN),
+		map(() => {
+			const userData: {
+				email: string;
+				id: string;
+				_token: string;
+				_tokenExpirationDate: string
+			} = JSON.parse(localStorage.getItem('userData'));
+			if (!userData) {
+				return { type: 'Dummy Action' };
+			}
+
+			const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+			if (loadedUser.token) {
+				return new AuthActions.AuthenticateSuccess({
+					email: loadedUser.email,
+					userId: loadedUser.id,
+					token: loadedUser.token,
+					expirationDate: new Date(userData._tokenExpirationDate)
+				});
+				// const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+				// this.autoLogout(expirationDuration);
+			}
+			return { type: 'Dummy Action' };
+		})
+	)
+
+	@Effect({ dispatch: false })
+	authLogout = this.actions$.pipe(
+		ofType(AuthActions.LOGOUT),
+		tap(() => {
+			localStorage.removeItem('userData');
+		})
+	)
 
 	constructor(private actions$: Actions, private http: HttpClient, private router: Router) { }
 }
