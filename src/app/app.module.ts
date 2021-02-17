0b775b9c9bb6608dfd227c0583ad0ac766491189
Dylan Hague <haguedylan@gmail.com>
2021-02-17 15:05:32 -0500

    Install Store-Devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 
+import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
@@ -13,20 +15,21 @@ import * as fromApp from './store/app.reducer';
 import { AuthEffects } from './auth/store/auth.effects';
 
 @NgModule({
-  declarations: [
+	declarations: [
 		AppComponent,
 		HeaderComponent,
-  ],
-  imports: [
+	],
+	imports: [
 		BrowserModule,
 		HttpClientModule,
 		AppRoutingModule,
 		StoreModule.forRoot(fromApp.appReducer),
+		StoreDevtoolsModule.instrument({ logOnly: environment.production }),
 		EffectsModule.forRoot([AuthEffects]),
 		SharedModule,
 		CoreModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
