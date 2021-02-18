756e316d0ff94f089023c5e1512ce4f61664aebc
Dylan Hague <haguedylan@gmail.com>
2021-02-18 11:00:42 -0500

    Create StroeRecipes Action/Effect

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,6 @@ import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { AuthService } from '../auth/auth.service';
-import { DataStorageService } from '../shared/data-storage.service';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from '../auth/store/auth.actions';
 import * as RecipeActions from '../recipes/store/recipe.actions';
@@ -17,19 +15,19 @@ export class HeaderComponent implements OnInit, OnDestroy {
 	isAuthenticated = false;
 	private userSub: Subscription;
 
-	constructor(private dataStorageService: DataStorageService, private authService: AuthService, private store: Store<fromApp.AppState>) {}
+	constructor(private store: Store<fromApp.AppState>) { }
 
-	ngOnInit () {
+	ngOnInit() {
 		this.userSub = this.store
-		.select('auth')
-		.pipe(map(authState => {return authState.user;}))
-		.subscribe(user => {
-			this.isAuthenticated = !!user; 
-		});
+			.select('auth')
+			.pipe(map(authState => { return authState.user; }))
+			.subscribe(user => {
+				this.isAuthenticated = !!user;
+			});
 	}
 
 	onSaveData() {
-		this.dataStorageService.storeRecipes();
+		this.store.dispatch(new RecipeActions.StoreRecipes());
 	}
 
 	onFetchData() {
--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Store } from "@ngrx/store";
 import { Actions, Effect, ofType } from "@ngrx/effects";
-import { map, switchMap } from "rxjs/operators";
+import { map, switchMap, withLatestFrom } from "rxjs/operators";
 
 import * as RecipesActions from './recipe.actions';
 import { Recipe } from "../recipe.model";
+import * as fromApp from '../../store/app.reducer';
 
 @Injectable()
 export class RecipeEffects {
@@ -30,5 +32,15 @@ export class RecipeEffects {
 			})
 		);
 
-	constructor(private actions$: Actions, private http: HttpClient) { }
+	@Effect({dispatch: false})
+	storeRecipes = this.actions$
+		.pipe(
+			ofType(RecipesActions.STORE_RECIPES),
+			withLatestFrom(this.store.select('recipes')),
+			switchMap( ([actionData, recipesState]) => {
+				return this.http.put('https://ng-recipe-book-6ae8d-default-rtdb.firebaseio.com/recipes.json', recipesState.recipes);
+			}),
+		);
+
+	constructor(private actions$: Actions, private http: HttpClient, private store: Store<fromApp.AppState>) { }
 }
