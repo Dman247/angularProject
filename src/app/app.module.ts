09a7fe3c363e9cd8234560d172faace5e41d28ec
Dylan Hague <haguedylan@gmail.com>
2021-02-18 08:42:01 -0500

    Create FetchRecipes Effect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import * as fromApp from './store/app.reducer';
 import { AuthEffects } from './auth/store/auth.effects';
+import { RecipeEffects } from './recipes/store/recipe.effects';
 
 @NgModule({
 	declarations: [
@@ -27,7 +28,7 @@ import { AuthEffects } from './auth/store/auth.effects';
 		StoreModule.forRoot(fromApp.appReducer),
 		StoreDevtoolsModule.instrument({ logOnly: environment.production }),
 		StoreRouterConnectingModule.forRoot(),
-		EffectsModule.forRoot([AuthEffects]),
+		EffectsModule.forRoot([AuthEffects, RecipeEffects]),
 		SharedModule,
 		CoreModule,
 	],
